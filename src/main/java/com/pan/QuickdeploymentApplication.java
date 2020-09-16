1cd7e1dca11b5e3e7104b853bdfa24cf753892c8
tangp666 <137889800@qq.com>
2020-09-16 15:56:27 +0800

    init project

---
+++ src/main/java/com/pan/QuickdeploymentApplication.java
@@ -0,0 +1,13 @@
+package com.pan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickdeploymentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuickdeploymentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pan/QuickdeploymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickdeploymentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
