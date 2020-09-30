e20516ef2de2197c7b2d734204eb26f34161bd33
tangp666 <137889800@qq.com>
2020-09-30 14:50:10 +0800

    测试文件清空

--- src/test/java/com/pan/QuickdeploymentApplicationTests.java
+++ src/test/java/com/pan/QuickdeploymentApplicationTests.java
@@ -14,40 +14,40 @@ import java.util.List;
 @SpringBootTest
 class QuickdeploymentApplicationTests {
 
-    @Autowired
-    private SysMenuService sysMenuService;
-
-    @Test
-    void contextLoads() {
+//    @Autowired
+//    private SysMenuService sysMenuService;
+//
+//    @Test
+//    void contextLoads() {
 //        List<SysMenuEntity> all = sysMenuService.findALL();
 //        System.out.println(all.size());
-    }
-
-    /**
-     * 下载zip文件
-     */
-    @Test
-    void testDownLoadNet(){
+//    }
+//
+//    /**
+//     * 下载zip文件
+//     */
+//    @Test
+//    void testDownLoadNet(){
 //        DownLoadUtils.downLoadNet("https://github.com/tangp666/QuickDeployment/archive/master.zip",
 //                "E:\\workspace\\QuickDeployment\\file\\QuickDeployment.zip");
-    }
-
-    /**
-     * 解压文件
-     */
-    @Test
-    void testUnZip(){
-//        ZipUtils.unZip("E:\\workspace\\QuickDeployment\\file\\QuickDeployment.zip");
-    }
-
-    /**
-     * mvn打包
-     */
-    @Test
-    void testMvnPackage(){
+//    }
+//
+//    /**
+//     * 解压文件
+//     */
+//    @Test
+//    void testUnZip(){
+////        ZipUtils.unZip("E:\\workspace\\QuickDeployment\\file\\QuickDeployment.zip");
+//    }
+//
+//    /**
+//     * mvn打包
+//     */
+//    @Test
+//    void testMvnPackage(){
 //        MVNUtils.mvnPackage("E:\\workspace\\QuickDeployment\\file\\QuickDeployment\\QuickDeployment-master\\pom.xml",
 //                //"mvn package",
 //                "clean package",
 //                "E:\\apache-maven-3.6.2");
-    }
+//    }
 }
